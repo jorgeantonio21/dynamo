7c9f5890c32efa372acafaec1bbdd7c05da33592
Jorge Antonio <matroid@outlook.com>
2025-08-04 13:42:28 +0100

    increase the expiration duration

--- tests/request_throttler/test_request_throttler_e2e_with_mockers.py
+++ tests/request_throttler/test_request_throttler_e2e_with_mockers.py
@@ -22,7 +22,7 @@ NUM_MOCKERS = 2
 BLOCK_SIZE = 16
 SPEEDUP_RATIO = 10.0
 FRONTEND_PORT = 8091
-REQUEST_THROTTLE_DURATION_MS = 3000  # 3 seconds for faster testing, this is the duration of the request throttling window
+REQUEST_THROTTLE_DURATION_MS = 5000  # 5 seconds for faster testing, this is the duration of the request throttling window
 MAX_QUEUE_DEPTH = 1  # Low threshold for easier testing
 
 
